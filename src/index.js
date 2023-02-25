9fdad8c51e074e70b5f12951fb475718fc1dbd3a
timopix <57803428+timopix@users.noreply.github.com>
2023-02-24 22:09:13 -0700

    feat: improved brackets, algorithm now quicker

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+function check(str, bracketsConfig) {
+  const stack = [];
+  const brackets = {};
+  const openBrackets = [];
+
+  for (const [open, close] of bracketsConfig) {
+    openBrackets.push(open);
+    brackets[close] = open;
+  }
+
+  function isOpenBracket(ch) {
+    return openBrackets.includes(ch);
+  }
+
+  for (const ch of str) {
+    const topElement = stack[stack.length - 1];
+    if (isOpenBracket(ch) && brackets[ch] !== ch) {
+      stack.push(ch);
+    } else if (isOpenBracket(ch) && brackets[ch] === ch) {
+      if (brackets[ch] === topElement) {
+        stack.pop();
+      } else {
+        stack.push(ch);
+      }
+    } else {
+      if (brackets[ch] === topElement) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
 }
+
+module.exports = check;
